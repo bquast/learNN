54f28ee01096e26926c88d9b12289849ecb4cf01
Bastiaan Quast <bquast@gmail.com>
2015-09-07 15:16:20 +0200

    actually test something

--- tests/testthat/test_learn_bp.R
+++ tests/testthat/test_learn_bp.R
@@ -12,3 +12,11 @@ y = matrix(c(0,
 
 # run full function
 learn_bp(X, y)
+
+# define context
+context("output format")
+
+test_that("output dimensions match", {
+  expect_equal( dim(syn0), c(3, 4) )
+  expect_equal( dim(syn1), c(4, 1) )
+})
